4894f4111dc04f996711f121bccff178b46e6ef1
Sérgio Jadir <sergio_jadir@outlook.com>
2013-05-20 22:12:21 -0300

    firts commit for test deploy capistrano

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '3ec62e3d82c19add4123b47a9b00e84e832d978ad3ca7a2a0e8a439e5684aeba3e2cd609b91712a5b80acc5cd5ef78e9c6d672d9229312fc6863a29f1c49a98e'
