6273dd269367761949fcb4b60c31b9bf1f513443
Sérgio Jadir <sergio_jadir@outlook.com>
2013-05-24 21:52:49 -0300

    clear json controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @people }
     end
   end
 
@@ -17,7 +16,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @person }
     end
   end
 
@@ -28,7 +26,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @person }
     end
   end
 
@@ -45,10 +42,8 @@ class PeopleController < ApplicationController
     respond_to do |format|
       if @person.save
         format.html { redirect_to @person, notice: 'Person was successfully created.' }
-        format.json { render json: @person, status: :created, location: @person }
       else
         format.html { render action: "new" }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -61,10 +56,8 @@ class PeopleController < ApplicationController
     respond_to do |format|
       if @person.update_attributes(params[:person])
         format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -77,7 +70,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to people_url }
-      format.json { head :no_content }
     end
   end
 end
